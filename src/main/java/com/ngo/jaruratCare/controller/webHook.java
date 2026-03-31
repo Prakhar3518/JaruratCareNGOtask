d49cf29843847482af997f7191433e434df68916
Prakhar Chaudhary <prakharchaudhary0302@gmail.com>
2026-03-31 17:33:43 +0530

    logger added

--- src/main/java/com/ngo/jaruratCare/controller/webHook.java
+++ src/main/java/com/ngo/jaruratCare/controller/webHook.java
@@ -2,6 +2,8 @@ package com.ngo.jaruratCare.controller;
 
 import com.ngo.jaruratCare.dto.messageRequest;
 import com.ngo.jaruratCare.service.chatService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class webHook {
+
+    private static final Logger logger = LoggerFactory.getLogger(webHook.class);
 
 
     private final chatService chatservice;
@@ -20,8 +24,16 @@ public class webHook {
     @PostMapping("/webhook")
     public String receiveMessage(@RequestBody messageRequest request){
         String msg = request.getMessage();
-        System.out.println("Received message:" + msg); //logging
 
-        return chatservice.getReply(msg);
+        // Log incoming message
+        logger.info("Received message: {}", msg);
+
+        String reply = chatservice.getReply(msg);
+
+        // Log outgoing response
+        logger.info("Sending reply: {}", reply);
+
+        return reply;
+
     }
 }
